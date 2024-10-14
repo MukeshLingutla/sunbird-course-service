0796774bf26746cdbfd588d0920c2cccb27b0e57
arpithasureshappa <arpitha.sureshappa@tarento.com>
2024-10-14 20:46:07 +0530

    conflicts issue resolved

--- course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -1152,6 +1152,6 @@ public final class JsonKey {
   public static final String EVENTID = "eventid";
   public static final String EVENT_REDIS_TTL = "eventRedisTTL";
   public static final String LEARNER_EVENT_DB = "learnerEvent_db";
-  public static final String LEARNER_ENROLMENT_DB = "learnerEnrolment_db";  public static final String EVENT_BATCH_DB = "eventBatchDB";
+  public static final String LEARNER_ENROLMENT_DB = "learnerEnrolment_db";
   private JsonKey() {}
 }
